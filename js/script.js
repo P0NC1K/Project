fac0ceaa2c195e33552c0a81b6bdd56e67b31fd7
Greg_Broflovski <ggherdelescu@mail.ru>
2021-02-14 01:16:07 +0200

    did something small

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let number = 0;
 
 window.addEventListener('scroll' , function() {
     if(window.scrollY > 1) {
-        header.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+        header.style.backgroundColor = "rgba(255, 255, 255, 0.7)";
         header.style.transition = "box-shadow .5s";
         header.style.boxShadow = "0 1px 8px 1px #131313";
     }
